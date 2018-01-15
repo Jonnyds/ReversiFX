e7be39ae3ea7af2f2d91518c24c91ba9166fe77f
NoamRI <noam.ri02gmail.com>
2018-01-15 19:11:51 +0200

    hey again

--- src/sample/SettController.java
+++ src/sample/SettController.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Separator;
 import javafx.scene.input.MouseEvent;
@@ -16,12 +17,28 @@ import static java.lang.System.exit;
 public class SettController implements Initializable {
     @FXML
     private GridPane setty;
+    @FXML
+    private ChoiceBox openPlayer;
+    @FXML
+    private ChoiceBox playerXcolor;
+    @FXML
+    private ChoiceBox playerOcolor;
+    @FXML
+    private ChoiceBox board_size;
+    @FXML
+    private Button BackButton;
 
 
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        openPlayer.setItems(FXCollections.observableArrayList("Player X", "Player O "));
+        playerXcolor.setItems(FXCollections.observableArrayList("Black", "Blue", "Yellow", "Pink", "Purple", "Cyan"));
+        playerOcolor.setItems(FXCollections.observableArrayList("White", "Blue", "Yellow", "Pink", "Purple", "Cyan"));
+        board_size.setItems(FXCollections.observableArrayList("4x4", "6x6", "8x8", "10x10", "12x12", "14x14", "16x16",
+                "18x18", "20x20"));
         ChoiceBox cb = new ChoiceBox();
         cb.setItems(FXCollections.observableArrayList(
                 "Player X", "Player O")
