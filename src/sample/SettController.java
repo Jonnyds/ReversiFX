335d1dff876f8bb5d46c17706f533632d3504ca0
jonnyds <schwarz.jonathan@icloud.com>
2018-01-15 20:34:08 +0200

    commit

--- src/sample/SettController.java
+++ src/sample/SettController.java
@@ -2,16 +2,18 @@ package sample;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Separator;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import static java.lang.System.exit;
 
 public class SettController implements Initializable {
     @FXML
@@ -28,8 +30,12 @@ public class SettController implements Initializable {
         );
 }
 
-    public void endGameEvent(MouseEvent mouseEvent) {
-        exit(0);
+    public void endGameEvent(MouseEvent mouseEvent) throws IOException {
+        Stage stage = (Stage) this.setty.getScene().getWindow();
+        VBox root = (VBox) FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        Scene s = new Scene(root, 520,400);
+        stage.setScene(s);
+        stage.show();
     }
 
 
