b15a6551a8320f879972c190572c7ae2bf9b0c87
NoamRI <noam.ri02gmail.com>
2018-01-16 18:10:17 +0200

    hey again

--- src/sample/SettController.java
+++ src/sample/SettController.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.collections.FXCollections;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,7 +12,10 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
+import javafx.scene.control.ColorPicker;
+import javafx.scene.paint.Color;
+import javafx.event.*;
+import java.awt.*;
 import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -21,9 +26,9 @@ public class SettController implements Initializable {
     @FXML
     private ChoiceBox openPlayer;
     @FXML
-    private ChoiceBox player1color;
+    private ColorPicker player1color;
     @FXML
-    private ChoiceBox player2color;
+    private ColorPicker player2color;
     @FXML
     private ChoiceBox board_size;
 
@@ -34,14 +39,21 @@ public class SettController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        ColorPicker colorPicker1 = new ColorPicker();
+        colorPicker1.setValue(Color.BLACK);
+        ColorPicker colorPicker2 = new ColorPicker();
+        colorPicker2.setValue(Color.WHITE);
+
         openPlayer.setItems(FXCollections.observableArrayList("Player 1", "Player 2"));
         openPlayer.getSelectionModel().selectFirst();
-        player1color.setItems(FXCollections.observableArrayList("Black","White", "Blue", "Yellow", "Pink",
+      /*  player1color.setItems(FXCollections.observableArrayList("Black","White", "Blue", "Yellow", "Pink",
+                "Purple", "Cyan"));*/
+
+        player1color.setValue(colorPicker1.getValue());
+        player2color.setValue(colorPicker2.getValue());
+        /*player2color.setItems(FXCollections.observableArrayList("Black","White", "Blue", "Yellow", "Pink",
                 "Purple", "Cyan"));
-        player1color.getSelectionModel().selectFirst();
-        player2color.setItems(FXCollections.observableArrayList("Black","White", "Blue", "Yellow", "Pink",
-                "Purple", "Cyan"));
-        player2color.getSelectionModel().select(1);
+        player2color.getSelectionModel().select(1);*/
         board_size.setItems(FXCollections.observableArrayList("4x4", "6x6", "8x8", "10x10", "12x12", "14x14", "16x16",
                 "18x18", "20x20"));
         board_size.getSelectionModel().select(2);
@@ -49,6 +61,8 @@ public class SettController implements Initializable {
         cb.setItems(FXCollections.observableArrayList(
                 "Player X", "Player O")
         );
+
+
 }
 
     public void backMenu(MouseEvent mouseEvent) throws IOException {
@@ -61,8 +75,8 @@ public class SettController implements Initializable {
         String open,color1,color2,size_b,size[];
 
         open = openPlayer.getSelectionModel().getSelectedItem().toString();
-        color1 = player1color.getSelectionModel().getSelectedItem().toString();
-        color2 = player2color.getSelectionModel().getSelectedItem().toString();
+        color1 = player1color.getValue().toString();
+        color2 = player2color.getValue().toString();
         size_b = board_size.getSelectionModel().getSelectedItem().toString();
         size = size_b.split("x");
 
